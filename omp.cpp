595efc1ea4f8e221810cb564e1c1356c8b9c9608
tdehoff1 <tdehoff@vols.utk.edu>
2025-11-25 12:20:47 -0500

    overlap file read and map: best version so far

--- omp.cpp
+++ omp.cpp
@@ -82,13 +82,47 @@ int hash_str(string s, int R) {
 void mapping_step() {
     unordered_map<string, size_t> buckets;
 
-    #pragma omp for schedule(dynamic)
-    for (size_t i = 0; i < readers_q.size(); ++i) {
-        buckets[readers_q[i]]++;
+    // Grab elemnts from the work q in chunks
+    const int chunk_size = 1024;  // find which chunk size works the best
+    vector<string> working_batch;
+    working_batch.reserve(chunk_size);
+
+    while (true) {
+        working_batch.clear();
+
+        // Lock and grab new chunk of elements if queue is not empty
+        omp_set_lock(&readers_lock);
+        for (size_t i = 0; i < chunk_size && !readers_q.empty(); ++i) {
+            working_batch.push_back(readers_q.back());
+            readers_q.pop_back();
+        }
+        omp_unset_lock(&readers_lock);
+
+        if (!working_batch.empty()) {
+            // Queue not empty -- process new elements
+            for (size_t i = 0; i < working_batch.size(); ++i) {
+                buckets[working_batch[i]]++;
+            }
+        }
+        else {
+            int remaining;
+            // Shared global variable -- must be read atomically
+            #pragma omp atomic read
+            remaining = files_remain;
+
+            if (remaining == 0) {
+                // Queue empty and all files are processed
+                break;
+            }
+            else {
+                // Mappers are ahead of readers
+                #pragma omp taskyield
+            }
+        }
     }
 
     // Push thread's results into the reducer queues
-    for (auto &el : buckets) {
+    for (auto el : buckets) {
         int index = hash_str(el.first, num_reducers);
         omp_set_lock(&reducer_locks[index]);
         reducer_queues[index].push(el);
@@ -118,12 +152,12 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int n_threads = 4;
-    omp_set_num_threads(n_threads);
+    int n_threads = omp_get_max_threads();
+    int num_mappers = n_threads;
+    num_reducers = n_threads * 2;  // Works best on my laptop -- test on ISAAC
+    files_remain = argc - 1;\
 
-    int num_mappers = n_threads / 2;
-    num_reducers = n_threads;
-    files_remain = argc - 1;
+    cerr << "Testing " <<  n_threads << " thread(s)\n";
 
     omp_init_lock(&readers_lock);
     omp_init_lock(&global_counts_lock);
@@ -133,14 +167,13 @@ int main(int argc, char* argv[]) {
     }
     reducer_queues.resize(num_reducers);
 
-    double start, end, start_f, end_f, start_m, end_m, start_r, end_r, start_p;
+    double start, end, start_r, start_p;
     start = omp_get_wtime();
 
     #pragma omp parallel
     {
         #pragma omp single
         {
-            start_f = omp_get_wtime();
             // File reading step
             size_t f_count = 1;
             while (argv[f_count]) {
@@ -150,15 +183,16 @@ int main(int argc, char* argv[]) {
                 }
                 f_count++;
             }
-        }
-        #pragma omp taskwait
-        end_f = omp_get_wtime();
 
-        start_m = omp_get_wtime();
-        // Mapping step
-        mapping_step();
+            // Mapping step
+            for (int i = 0; i < num_mappers; ++i) {
+                #pragma omp task
+                {
+                    mapping_step();
+                }
+            }
+        }
     }
-    end_m = omp_get_wtime();
 
     start_r = omp_get_wtime();
     // Reducing step
@@ -166,7 +200,6 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < num_reducers; ++i) {
         reduce_step(i);
     }
-    end_r = omp_get_wtime();
 
     start_p = omp_get_wtime();
     vector<pair<string, size_t>> counts;
@@ -189,9 +222,8 @@ int main(int argc, char* argv[]) {
     end = omp_get_wtime();
     // Use cerr to always print in terminal
     cerr << "OpenMP time: " << (end - start) * 1000 << " ms\n";
-    cerr << "  File reading time: " << (end_f - start_f) * 1000 << " ms\n";
-    cerr << "  Mapping time: " << (end_m - start_m) * 1000 << " ms\n";
-    cerr << "  Reducing time: " << (end_r - start_r) * 1000 << " ms\n";
+    cerr << "  File read & Map time: " << (start_r - start) * 1000 << " ms\n";
+    cerr << "  Reducing time: " << (start_p - start_r) * 1000 << " ms\n";
     cerr << "  Sort & Print time: " << (end - start_p) * 1000 << " ms\n";
 
     omp_destroy_lock(&readers_lock);
