0de8288fde92b68d0b7c57d2fc05742b7f4d24bc
Holden Roaten <160648193+hroaten@users.noreply.github.com>
2025-12-01 12:34:05 -0500

    Add SLURM job script for sequential processing

---
+++ seq_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J final-seq   #Job name
+#SBATCH -A acf-utk0011  #Write your project account associated to utia condo
+#SBATCH -p short
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8  #--ntasks-per-node is used when we want to define the number of processes per node
+#SBATCH --cpus-per-task=4
+#SBATCH --time=00:20:00
+#SBATCH -o seq.o%j
+#SBATCH --qos=short
+
+TEST_DIR="raw_text_input"
+
+args=()
+
+for file in "$TEST_DIR"/*; do
+    args+=("$file")
+done
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+./seq "${args[@]}" > seq_out.txt
